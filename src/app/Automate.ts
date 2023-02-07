9abc59a641abf6b1d55201197ecd41fc8eb71d76
THIEBAUT Hugo <hugo.thiebaut6@etu.univ-lorraine.fr>
2023-02-07 12:52:35 +0100

    Commit initial + Automate fini et Tests aussi

---
+++ src/app/Automate.ts
@@ -0,0 +1,76 @@
+export class Automate {
+  static valideCalcul(calcul: string): true | [number, string] {
+    function estChiffre(chiffre: string): boolean {
+      if (["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"].includes(chiffre)) return true;
+      return false;
+    }
+    let index = 0;
+
+    function etat_a(input: string, index: number): true | [number, string] {
+      if (estChiffre(input[index])) {
+        return etat_c(input, index + 1);
+      } else if (input[index] === "-") {
+        return etat_b(input, index + 1);
+      } else if (input[index] === ".") {
+        return etat_d(input, index + 1);
+      }
+      return [index, "numérique attendu"];
+    }
+
+    function etat_b(input: string, index: number): true | [number, string] {
+      if (estChiffre(input[index])) {
+        return etat_c(input, index + 1);
+      } else if (input[index] === ".") {
+        return etat_d(input, index + 1);
+      }
+      return [index, "numérique attendu"];
+    }
+
+    function etat_c(input: string, index: number): true | [number, string] {
+      if (estChiffre(input[index])) {
+        return etat_c(input, index + 1);
+      } else if (
+        input[index] === "+" ||
+        input[index] === "-" ||
+        input[index] === "*" ||
+        input[index] === "/"
+      ) {
+        return etat_a(input, index + 1);
+      } else if (input[index] === ".") {
+        return etat_e(input, index + 1);
+      } else if (input[index] === "=") {
+        return true;
+      }
+      return [index, "numérique ou opérateur attendu"];
+    }
+
+    function etat_d(input: string, index: number): true | [number, string] {
+      if (estChiffre(input[index])) {
+        return etat_e(input, index + 1);
+      }
+      return [index, "numérique attendu"];
+    }
+
+    function etat_e(input: string, index: number): true | [number, string] {
+      if (estChiffre(input[index])) {
+        return etat_e(input, index + 1);
+      } else if (
+        input[index] === "+" ||
+        input[index] === "-" ||
+        input[index] === "*" ||
+        input[index] === "/"
+      ) {
+        return etat_a(input, index + 1);
+      } else if (input[index] === "=") {
+        return true;
+      }
+      return [index, "numérique ou opérateur attendu"];
+    }
+
+    return etat_a(calcul, index);
+  }
+}
+
+let result = Automate.valideCalcul("1+1=2");
+
+console.log(result);
---
+++ src/tests/Automate.test.ts
@@ -0,0 +1,127 @@
+import { Automate } from "../app/Automate";
+
+// Tests d'opérations valides
+
+//Test des 4 opérations basiques
+
+test("1+1=2", () => {
+  expect(Automate.valideCalcul("1+1=2")).toBe(true);
+});
+
+test("1-1=0", () => {
+  expect(Automate.valideCalcul("1-1=0")).toBe(true);
+});
+
+test("1*1=1", () => {
+  expect(Automate.valideCalcul("1*1=1")).toBe(true);
+});
+
+test("1/1=1", () => {
+  expect(Automate.valideCalcul("1/1=1")).toBe(true);
+});
+
+//Test des 4 opérations basiques avec chiffre flotants
+
+test(".1+.1=.2", () => {
+  expect(Automate.valideCalcul(".1+.1=.2")).toBe(true);
+});
+
+test(".1-.1=0", () => {
+  expect(Automate.valideCalcul(".1-.1=0")).toBe(true);
+});
+
+test(".1*.1=.01", () => {
+  expect(Automate.valideCalcul(".1*.1=.01")).toBe(true);
+});
+
+test(".1/.1=1", () => {
+  expect(Automate.valideCalcul(".1/.1=1")).toBe(true);
+});
+
+//Test avec nombre flottants
+
+test("11.11+1=12.11", () => {
+  expect(Automate.valideCalcul("11.11+1=12.11")).toBe(true);
+});
+
+//Test des 4 opérations basiques avec chiffre négatif
+
+test("-1+-1=-2", () => {
+  expect(Automate.valideCalcul("-1+-1=2")).toBe(true);
+});
+
+test("-1--1=0", () => {
+  expect(Automate.valideCalcul("-1--1=0")).toBe(true);
+});
+
+test("-1*-1=1", () => {
+  expect(Automate.valideCalcul("-1*-1=0")).toBe(true);
+});
+
+test("1/1=1", () => {
+  expect(Automate.valideCalcul("-1/-1=1")).toBe(true);
+});
+
+//Test des 4 opérations basiques avec chiffre négatif et chiffre flottant
+
+test("-.1+-.1=-.2", () => {
+  expect(Automate.valideCalcul("-.1+-.1=-.2")).toBe(true);
+});
+
+test("-.1--.1=0", () => {
+  expect(Automate.valideCalcul("-.1--.1=0")).toBe(true);
+});
+
+test("-.1*-.1=.01", () => {
+  expect(Automate.valideCalcul("-.1*-.1=.01")).toBe(true);
+});
+
+test("-.1/-.1=1", () => {
+  expect(Automate.valideCalcul("-.1/-.1=1")).toBe(true);
+});
+
+//Test des 4 opérations basiques avec un NOMBRE
+
+test("10+10=20", () => {
+  expect(Automate.valideCalcul("10+10=20")).toBe(true);
+});
+
+test("10-10=0", () => {
+  expect(Automate.valideCalcul("10-10=0")).toBe(true);
+});
+
+test("10*10=100", () => {
+  expect(Automate.valideCalcul("10*10=100")).toBe(true);
+});
+
+test("10/10=1", () => {
+  expect(Automate.valideCalcul("10/10=1")).toBe(true);
+});
+
+// Test avec des erreurs
+
+test("a+1=1 | état a, index 0", () => {
+  expect(Automate.valideCalcul("a+1=1")).toStrictEqual([0, "numérique attendu"]);
+});
+
+test("--1+1=1 | état b, index 1", () => {
+  expect(Automate.valideCalcul("--1+1=1")).toStrictEqual([1, "numérique attendu"]);
+});
+
+test("1111a | état c, index 4", () => {
+  expect(Automate.valideCalcul("1111a")).toStrictEqual([4, "numérique ou opérateur attendu"]);
+});
+
+test(".- | état d, index 1", () => {
+  expect(Automate.valideCalcul(".-")).toStrictEqual([1, "numérique attendu"]);
+});
+
+test(".1a | état e, index 2", () => {
+  expect(Automate.valideCalcul(".1a")).toStrictEqual([2, "numérique ou opérateur attendu"]);
+});
+
+// Tests avec 2 operateurs
+
+test("1 + 1 = 1", () => {
+  expect(Automate.valideCalcul("1 + 1 = 1")).toBe(true);
+});
